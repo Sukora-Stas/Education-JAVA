91180567198ef296612baa7e75ae82b8e6bbbc48
Sukora-Stas <stasiksukora@mail.ru>
2016-11-25 15:57:32 +0300

    commit geekbrains

---
+++ src/geekbrains/lesson_1/helloWorld.java
@@ -0,0 +1,11 @@
+package geekbrains.lesson_1;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Я буду изучать JAVA");
+    }
+}
