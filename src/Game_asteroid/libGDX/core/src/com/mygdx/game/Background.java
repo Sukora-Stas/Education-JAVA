ac10721df9d35de8842fb1da033f51ac1ac6909f
Sukora-Stas <stasiksukora@mail.ru>
2016-11-27 22:36:16 +0300

    libGDX add Background

---
+++ src/Game_asteroid/libGDX/core/src/com/mygdx/game/Background.java
@@ -0,0 +1,56 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Background {
+    class Star {
+        private Vector2 position; // позиция Х и У
+        private float speed;       //скорость движения
+
+        public Star() {
+            position = new Vector2((float) Math.random() * 1280, (float) Math.random() * 720);
+            speed = 1.0f + (float) Math.random() * 9.0f;  //разная скорость
+            //speed = 5.0f;
+        }
+
+        public void update() {
+            position.x -= speed;
+            if (position.x < -40) {
+                position.x = 1300;
+                position.y = (float) Math.random() * 720;
+            }
+        }
+    }
+
+    private final int STARS_COUNT = 200;
+    private Star[] stars;
+    private Texture texture;
+    private Texture textureStar;
+
+    public Background() {
+        texture = new Texture("staticback.jpg");
+        textureStar = new Texture("star12.tga");
+        stars = new Star[STARS_COUNT];
+        for (int i = 0; i < STARS_COUNT; i++) {
+            stars[i] = new Star();
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, 0, 0);
+        for (int i = 0; i < STARS_COUNT; i++) {
+            batch.draw(textureStar, stars[i].position.x, stars[i].position.y);
+        }
+    }
+
+    public void update() {
+        for (int i = 0; i  < STARS_COUNT; i++) {
+            stars[i].update();
+        }
+    }
+}
--- src/Game_asteroid/libGDX/core/src/com/mygdx/game/MyGdxGame.java
+++ src/Game_asteroid/libGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -8,26 +8,30 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Background bg;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("bullet20.png");
+		bg = new Background();
 	}
 
 	@Override
 	public void render () {
+		update();
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 15, 15);
+		bg.render(batch);
 		batch.end();
 	}
-	
+
+	public void update(){
+		bg.update();
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
