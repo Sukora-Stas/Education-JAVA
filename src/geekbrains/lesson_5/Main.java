c746d6faffbd5b0a21b550fc619bd2bbfecfc822
Sukora-Stas <stasiksukora@mail.ru>
2016-12-09 15:22:41 +0300

    lesson 5

--- src/geekbrains/lesson_5/Main.java
+++ src/geekbrains/lesson_5/Main.java
@@ -10,14 +10,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ваша задаач угадать число.");
-        for (int i = 10; i <= 30; i += 10) {
-            int number = (int) (Math.random() * i);
-            playLevel(i, number);
-        }
+        for (int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выйграли!!!");
         scanner.close();
     }
 
-    private static void playLevel(int range, int number) {
+    private static void playLevel(int range) {
+        int number = (int) (Math.random() * range);
         System.out.println("Угадайте число от 0 до " + range);
         while (true) {
             int input_number = scanner.nextInt();
