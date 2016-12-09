a706b8d09c0eb74e2c595d6117983e0a033d1c6f
Sukora-Stas <stasiksukora@mail.ru>
2016-12-09 16:14:27 +0300

    catch the drop

--- src/geekbrains/catch_the_drop/GameWindow.java
+++ src/geekbrains/catch_the_drop/GameWindow.java
@@ -23,8 +23,8 @@ public class GameWindow extends JFrame {
 
     private static void onRepaint(Graphics g) {
         g.fillOval(10, 10, 200, 100);
-        g.fillRect(200,200,100,2);
-
+        g.fillRect(150,150,100,2);
+        g.drawLine(200,200,400,300);
     }
 
     private static class GameFields extends JPanel {
