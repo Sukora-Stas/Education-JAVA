27994110eec693a1c4320231a9f3279662f4d9b6
Sukora-Stas <stasiksukora@mail.ru>
2017-01-18 18:24:27 +0300

    lesson 1

---
+++ src/DevColibri/HelloWorld.java
@@ -0,0 +1,15 @@
+package DevColibri;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n");
+        not();
+    }
+
+    static void not(){
+        System.out.println("Not working!!!");
+    }
+}
