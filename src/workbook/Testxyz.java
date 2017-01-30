8b6308cf2920423d2b919530c01ebe47f8d8493b
Sukora-Stas <stasiksukora@mail.ru>
2017-01-30 11:25:20 +0300

    test

---
+++ src/workbook/Testxyz.java
@@ -0,0 +1,13 @@
+package workbook;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Testxyz {
+    public static void main(String[] args) {
+        double x = 2;
+        double y = 0;
+        double z = x / y;
+        System.out.println(z);
+    }
+}
