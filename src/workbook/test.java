fd8a0fe46bf37432ff166f73b9ed005b22a6127d
Sukora-Stas <stasiksukora@mail.ru>
2017-02-23 14:03:02 +0300

    test

--- src/workbook/test.java
+++ src/workbook/test.java
@@ -1,7 +1,21 @@
 package workbook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Created by Sukora Stas.
  */
 public class test {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            names.add(String.valueOf(i));
+        }
+        System.out.println(names);
+        for (int j = 0; j < names.size(); j++) {
+            names.add(j, "Updated Name");
+        }
+        System.out.println(names);
+    }
 }
