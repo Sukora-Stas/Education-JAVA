07a6fe9d308adc2f293f58885aad572c6def6424
Sukora-Stas <stasiksukora@mail.ru>
2017-05-04 22:39:43 +0300

    task 18 done

--- src/workbook/Task/Task18.java
+++ src/workbook/Task/Task18.java
@@ -9,29 +9,24 @@ import java.util.GregorianCalendar;
 
 
 /*
-* Имеется строка с текстом. Подсчитать количество слов в тексте. Желательно
-* учесть, что слова могут разделяться несколькими пробелами, в начале и конце
- * текста также могут быть пробелы, но могут и отсутствовать.
+* Найти в строке не только запятые, но и другие знаки припения. Подсчитать общее их колличество
 * */
 public class Task18 {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println(startTime);
         char c;
         int sum = 0;
-        String str = "Your time is limited, so don’t waste it living someone else’s life";
+        String str = "Your time is limited, so don’t waste it living someone else’s life!";
         for (int i = 0; i < str.length(); i++) {
             c = str.charAt(i);
-            if (c == ' ' || c == ',') {
+            if (c == ',' || c == '–' || c == ':' || c == ')' || c == '(' || c == ';' || c == '.' || c == '«' || c == '»' || c == '!' || c == '\'') {
                 sum++;
             }
         }
 
         System.out.println("Колличество слов = " + sum);
         long timeSpent = System.currentTimeMillis();
-        System.out.println(timeSpent);
-
         System.out.println("программа выполнялась " + (timeSpent - startTime) + " миллисекунд");
     }
 }
