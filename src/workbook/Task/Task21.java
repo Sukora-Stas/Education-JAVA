25926b70fe3ae34625ee10b37ac3811d1f1a655d
Sukora-Stas <stasiksukora@mail.ru>
2017-05-05 17:59:51 +0300

    Task 21

--- src/workbook/Task/Task21.java
+++ src/workbook/Task/Task21.java
@@ -8,46 +8,98 @@ package workbook.Task;
 * с помощью оператора сложения и String, а другой с помощью StringBuilder и метода append.
 * Сравните скорость их выполнения.*/
 public class Task21 {
-    public static long measure1() {
-        String firstString = "";
-        long ft, fe;
-        ft = System.nanoTime();
-        for (int i = 0; i < 20; i++) {
-            firstString += "Сравнить скорость выполнения";
-        }
-        fe = System.nanoTime();
-        return fe - ft;
-    }
 
-    public static long measure2() {
-        StringBuilder secondString = new StringBuilder();
-        long st, en;
-        st = System.nanoTime();
-        for (int i = 0; i < 20; i++) {
-            secondString.append("Сравнить скорость выполнения");
-        }
-        en = System.nanoTime();
-        return en - st;
-    }
-    public static void main(String[] args) {
-
-        long startTime = System.currentTimeMillis();
-        measure1();
-        long timeSpent = System.currentTimeMillis();
-        System.out.println("сложение заняла " + (timeSpent - startTime) + " миллисекунд");
-        System.out.println();
-        startTime = System.currentTimeMillis();
-        measure1();
-        timeSpent = System.currentTimeMillis();
-        System.out.println("Операция append заняла " + (timeSpent - startTime) + " миллисекунд");
-
-
-        System.out.println("JVM тут еще не скомпилировала - Загрузка в String " + String.format("%,12d", measure1()) + " ns");
-        System.out.println("Второй вызов, на разогретой JVM - Загрузка в String " + String.format("%,12d", measure1()) + " ns");
-
-        System.out.println("JVM тут еще не скомпилировала - Загрузка в билдер " + String.format("%,12d", measure2()) + " ns");
-        System.out.println("Второй вызов, на разогретой JVM - Загрузка в билдер " + String.format("%,12d", measure2()) + " ns");
+    public static long stringPlus(){
+        String firstStr="";
+        Long fs, fe;
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static long measure1() {
+//        String firstString = "";
+//        long ft, fe;
+//        ft = System.nanoTime();
+//        for (int i = 0; i < 20; i++) {
+//            firstString += "Сравнить скорость выполнения";
+//        }
+//        fe = System.nanoTime();
+//        return fe - ft;
+//    }
+//
+//    public static long measure2() {
+//        StringBuilder secondString = new StringBuilder();
+//        long st, en;
+//        st = System.nanoTime();
+//        for (int i = 0; i < 20; i++) {
+//            secondString.append("Сравнить скорость выполнения");
+//        }
+//        en = System.nanoTime();
+//        return en - st;
+//    }
+//    public static void main(String[] args) {
+//
+//        long startTime = System.currentTimeMillis();
+//        measure1();
+//        long timeSpent = System.currentTimeMillis();
+//        System.out.println("сложение заняла " + (timeSpent - startTime) + " миллисекунд");
+//        System.out.println();
+//        startTime = System.currentTimeMillis();
+//        measure1();
+//        timeSpent = System.currentTimeMillis();
+//        System.out.println("Операция append заняла " + (timeSpent - startTime) + " миллисекунд");
+//
+//
+//        System.out.println("JVM тут еще не скомпилировала - Загрузка в String " + String.format("%,12d", measure1()) + " ns");
+//        System.out.println("Второй вызов, на разогретой JVM - Загрузка в String " + String.format("%,12d", measure1()) + " ns");
+//
+//        System.out.println("JVM тут еще не скомпилировала - Загрузка в билдер " + String.format("%,12d", measure2()) + " ns");
+//        System.out.println("Второй вызов, на разогретой JVM - Загрузка в билдер " + String.format("%,12d", measure2()) + " ns");
+//
+//
+//    }
 }
