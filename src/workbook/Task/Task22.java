5b63ed6b08c3e8bb85880b05fb5b47def467021e
Sukora-Stas <stasiksukora@mail.ru>
2017-05-21 21:08:09 +0300

    Task 22

--- src/workbook/Task/Task22.java
+++ src/workbook/Task/Task22.java
@@ -16,20 +16,56 @@ public class Task22 {
     private int m;
     private int h;
 
-    public void setTime(int h, int m, int s) {
+    private void setTime(int h, int m, int s) {
         this.s = s;
         this.m = m;
         this.h = h;
     }
 
-    public String getTime() {
+    private String getTime() {
         return "Hour: " + h + " Minute: " + m + " Second: " + s;
     }
 
+    private String getSecond() {
+        int sum = 0;
+
+        sum += h * 60 * 60;
+        sum += m * 60;
+        sum += s;
+
+        return "Second full = " + sum;
+    }
+
+    public Task22(int fullSec1) {
+        fullSec = fullSec1;
+    }
+
+    public Task22(int sec1, int min1, int hour1) {
+        s = sec1;
+        m = min1;
+        h = hour1;
+
+    }
+
+    static int compareToo(int obj1, int obj2) {
+        if (obj1 < obj2)
+            return -1;
+        else if (obj1 > obj2)
+            return 1;
+        else
+            return 0;
+    }
 
     public static void main(String[] args) {
         Task22 time = new Task22();
+
         time.setTime(10, 20, 50);
+
         System.out.println(time.getTime());
+
+        System.out.println(time.getSecond());
+
+int result = compareToo()
+
     }
 }
