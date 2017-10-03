8b65a46d88f1a2b2a580b78a79a402d24277ad79
Stas Sukora <stasiksukora@mail.ru>
2017-10-03 19:42:20 +0300

    OS lab_4

--- src/UniversityStudy/src/OS/lab_4/Main.java
+++ src/UniversityStudy/src/OS/lab_4/Main.java
@@ -19,29 +19,35 @@ public class Main {
         int n = Integer.parseInt(reader.readLine());
 
         double[] y = new double[K - 1];
-
+//input
         for (int i = 0; i < y.length; i++) {
-            double derivative = derivative(n,N);
-//            String formattedDouble = String.format("%.10f", test);
-            String formattedDouble = new DecimalFormat("#0.0000000000").format(derivative);
-            System.out.println(formattedDouble);
-
-
+            double derivative = derivative(i,N,n);
             y[i] = derivative;
+        }
+
+//output
+        for (int i = 0; i < y.length; i++) {
+            String formattedDouble = new DecimalFormat("#0.00000").format(y[i]);
+            System.out.println(formattedDouble);
         }
 
 
     }
 
-    public static double derivative(int stepen, int N) {
+    //stepTeilor
+    public static double derivative(int stepen, int N,int n) {
         if (stepen == 0 || stepen % 2 == 0) {
             return 0;
         }
         if (stepen == 1) {
-            return (2 * ((Math.PI) / N)) / factorial(stepen);
+            return (2 * ((Math.PI) / ((Math.pow(Math.PI, stepen))) / factorial(stepen)));
+        }
+        double m=0;
+        for (int i = 0; i < n; i++) {
+            m+=((Math.pow(2, stepen) * -1) * ((Math.pow(Math.PI, stepen)) / Math.pow(N,stepen))) / factorial(stepen);
         }
 
-        return ((Math.pow(2, stepen) * -1) * ((Math.pow(Math.PI, stepen)) / Math.pow(N,stepen))) / factorial(stepen);
+        return m;
     }
 
     public static int factorial(int a) {
@@ -54,34 +60,4 @@ public class Main {
     }
 
 
-
-       /* //output
-        for (int i = 0; i < y.length; i++) {
-            System.out.println(y[i]);
-        }*/
-
-
-
-
-      /*  double step = (Math.PI / 2) / ((N - 1) / 2);
-        System.out.println("--------------------------------------------------------");
-        System.out.println("|     x     |   sin(x)   | tailor_sin(x) |  difference  |");
-        System.out.println("--------------------------------------------------------");
-        for (double i = (-Math.PI / 2); i <= (Math.PI / 2); i += step) {
-            double math_sin = Math.sin(step);
-            double tailor_sin = i - Math.pow(i, 3.0) / factorial(3.0) + Math.pow(i, 5.0) / factorial(5.0);
-            double diff = math_sin - tailor_sin;
-            System.out.printf("| %1.6f |  %1.6f  |   %1.6f   |   %1.6f   |", i,
-                    math_sin, tailor_sin, diff);
-            System.out.println();
-        }
-    }
-
-    public static double factorial(Double n) {
-        double ret = 1.0;
-        for (double i = 1.0; i <= n; ++i)
-            ret *= i;
-        return ret;
-    }*/
-    //}
 }
