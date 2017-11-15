40406a604ac6e31848d2b01b4099020f40e6642e
Sukora Stas <stasiksukora@mail.ru>
2017-11-15 22:33:01 +0300

    KP_lab_2

--- src/UniversityStudy/src/Kprog/lab2.java
+++ src/UniversityStudy/src/Kprog/lab2.java
@@ -3,5 +3,19 @@ package UniversityStudy.src.Kprog;
 /**
  * Created by Sukora Stas.
  */
+
+/*
+*  Реализовать класс String для работы со строками символов.
+ *  Перегрузить операторы !(пуст ли String), ()(int,int) – возвращение подстроки, [].
+  *  Предоставить конструктор копирования. Определить friend функции для операций ввода-вывода в поток.
+*  */
 public class lab2 {
+    public static void main(String[] args) {
+        String string = "dgafgafg \n" +
+                "gsfgfsdgafgaf \n" +
+                "sdfasfdgbvafdb \n" +
+                "grgafgvdfab \n";
+
+        System.out.println(string.indexOf(string,10));
+    }
 }
